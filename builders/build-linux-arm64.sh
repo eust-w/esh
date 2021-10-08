aa54bcca5eb9fc2ca66f4403821a6da796df33a7
eust-w <eustancewu@gmail.com>
2021-10-08 18:17:59 +0800

    fix bug

--- builders/build-linux-arm64.sh
+++ builders/build-linux-arm64.sh
@@ -6,7 +6,7 @@ go env -w GOARCH=arm64
 go build -ldflags '-w -s' -gcflags '-l' -a -o esh-linux-arm64
 upx -9 esh-linux-arm64
 mv ./esh-linux-arm64 ./pkg/esh-linux-arm64
-chmod 777 .pkg/esh-linux-arm64
+chmod 777 pkg/esh-linux-arm64
 go env -w CGO_ENABLED=1
 go env -w GOOS=linux
 go env -w GOARCH=amd64
